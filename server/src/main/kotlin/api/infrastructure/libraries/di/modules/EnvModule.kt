91509e8594129312d3989f41aecef84a7d6912b7
Anthony Limon <pro.anthony.limon@gmail.com>
2024-09-06 01:11:40 +0200

    Remove useless debug

--- server/src/main/kotlin/api/infrastructure/libraries/di/modules/EnvModule.kt
+++ server/src/main/kotlin/api/infrastructure/libraries/di/modules/EnvModule.kt
@@ -7,87 +7,48 @@ import org.koin.dsl.module
 
 val envModule = module {
     single {
-        try {
-            println("API_HOST ${System.getenv("API_HOST")}")
-            EnvApi(
-                host = getRequiredEnv(EnvKey.API_HOST),
-                port = getRequiredEnv(EnvKey.API_PORT).toInt()
-            )
-        } catch (e: Exception) {
-            println("Error")
-            println(e.cause.toString())
-            println(e.stackTraceToString())
-            println(e.message)
-            throw e
-        }
+        EnvApi(
+            host = getRequiredEnv(EnvKey.API_HOST),
+            port = getRequiredEnv(EnvKey.API_PORT).toInt()
+        )
     }
 
     single {
-        try {
-            EnvApplication(
-                environment = EnvironmentType.fromValue(getRequiredEnv(EnvKey.APPLICATION_ENVIRONMENT)),
-            )
-        } catch (e: Exception) {
-            println("Error")
-            println(e.message)
-            throw e
-        }
+        EnvApplication(
+            environment = EnvironmentType.fromValue(getRequiredEnv(EnvKey.APPLICATION_ENVIRONMENT)),
+        )
     }
 
     single {
-        try {
-            EnvJwt(
-                secret = getRequiredEnv(EnvKey.JWT_SECRET),
-                refreshExpiration = getRequiredEnv(EnvKey.JWT_REFRESH_EXPIRATION).toInt(),
-                accessExpiration = getRequiredEnv(EnvKey.JWT_ACCESS_EXPIRATION).toInt(),
-            )
-        } catch (e: Exception) {
-            println("Error")
-            println(e.message)
-            throw e
-        }
+        EnvJwt(
+            secret = getRequiredEnv(EnvKey.JWT_SECRET),
+            refreshExpiration = getRequiredEnv(EnvKey.JWT_REFRESH_EXPIRATION).toInt(),
+            accessExpiration = getRequiredEnv(EnvKey.JWT_ACCESS_EXPIRATION).toInt(),
+        )
     }
 
     single {
-        try {
-            EnvDatabase(
-                host = getRequiredEnv(EnvKey.DATABASE_HOST),
-                port = getRequiredEnv(EnvKey.DATABASE_PORT).toInt(),
-                name = getRequiredEnv(EnvKey.DATABASE_NAME),
-                user = getRequiredEnv(EnvKey.DATABASE_USER),
-                password = getRequiredEnv(EnvKey.DATABASE_PASSWORD),
-            )
-        } catch (e: Exception) {
-            println("Error")
-            println(e.message)
-            throw e
-        }
+        EnvDatabase(
+            host = getRequiredEnv(EnvKey.DATABASE_HOST),
+            port = getRequiredEnv(EnvKey.DATABASE_PORT).toInt(),
+            name = getRequiredEnv(EnvKey.DATABASE_NAME),
+            user = getRequiredEnv(EnvKey.DATABASE_USER),
+            password = getRequiredEnv(EnvKey.DATABASE_PASSWORD),
+        )
     }
 
     single {
-        try {
-            EnvS3(
-                accessKey = getRequiredEnv(EnvKey.S3_ACCESS_KEY),
-                secretKey = getRequiredEnv(EnvKey.S3_SECRET_KEY),
-                bucketName = getRequiredEnv(EnvKey.S3_BUCKET_NAME),
-                region = getRequiredEnv(EnvKey.S3_REGION),
-                endpointUrl = getEnv(EnvKey.S3_ENDPOINT_URL),
-                endpointPort = getEnv(EnvKey.S3_ENDPOINT_PORT)?.toInt()
-            )
-        } catch (e: Exception) {
-            println("Error")
-            println(e.message)
-            throw e
-        }
+        EnvS3(
+            accessKey = getRequiredEnv(EnvKey.S3_ACCESS_KEY),
+            secretKey = getRequiredEnv(EnvKey.S3_SECRET_KEY),
+            bucketName = getRequiredEnv(EnvKey.S3_BUCKET_NAME),
+            region = getRequiredEnv(EnvKey.S3_REGION),
+            endpointUrl = getEnv(EnvKey.S3_ENDPOINT_URL),
+            endpointPort = getEnv(EnvKey.S3_ENDPOINT_PORT)?.toInt()
+        )
     }
 
     single {
-        try {
-            Env(get(), get(), get(), get(), get())
-        } catch (e: Exception) {
-            println("Error")
-            println(e.message)
-            throw e
-        }
+        Env(get(), get(), get(), get(), get())
     }
 }
